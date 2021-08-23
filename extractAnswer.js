102cc667e8e835ae72bfad1699d68e89cad6d818
davgatseren <davgatseren@gmail.com>
2021-08-23 13:11:30 +0800

    changed convertor

--- extractAnswer.js
+++ extractAnswer.js
@@ -1,30 +1,36 @@
-const csv = require('csv-parser');
-const fs = require('fs');
+const csv = require("csv-parser");
+const fs = require("fs");
 
+var id = 0;
 const results = [];
-fs.createReadStream('./answers.csv')
-  .pipe(csv())
-  .on('data', (data) => {
-    const question_id = data['№'];
-    const answer = data["Tsoomoo's version"];
+fs.createReadStream("./answers.csv")
+    .pipe(csv())
+    .on("data", (data) => {
+        const question_id = data["№"];
+        const answer = data["Answer"];
 
-    if (
-      answer === 'check' ||
-      answer === 'Monday' ||
-      answer === 'no correct answer (within 5 w/d)' ||
-      answer === 'a-incorrect question'
-    ) {
-      console.log('skipping');
-    } else {
-      answer.split('').forEach((each) => {
-        results.push({
-          question_id,
-          answer: each,
-        });
-      });
-    }
-  })
-  .on('end', () => {
-    console.log(results);
-    fs.writeFileSync('correct_answers_parsed.json', JSON.stringify(results));
-  });
+        if (
+            answer === "check" ||
+            answer === "Monday" ||
+            answer === "no correct answer (within 5 w/d)" ||
+            answer === "a-incorrect question"
+        ) {
+            console.log("skipping");
+        } else {
+            answer.split("").forEach((each) => {
+                results.push({
+                    id,
+                    test_id: question_id,
+                    answer: each,
+                });
+            });
+            id++;
+        }
+    })
+    .on("end", () => {
+        console.log(results);
+        fs.writeFileSync(
+            "correct_answers_parsed_test.json",
+            JSON.stringify(results),
+        );
+    });
--- extractQuestion.js
+++ extractQuestion.js
@@ -1,69 +1,73 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream('./questions.txt'),
+const lineReader = require("readline").createInterface({
+    input: require("fs").createReadStream("./questions.txt"),
 });
 
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 const parseQuestions = async (callback) => {
-  const questions = [];
-  const options = [];
-  let question_id;
-  let alphabetIndex = 0;
+    const questions = [];
+    const options = [];
+    let question_id;
+    let answer_id = 0;
+    let alphabetIndex = 0;
 
-  lineReader.on('line', (line) => {
-    const lineSplit = line.replace(/\t/g, '').split(' ');
+    lineReader.on("line", (line) => {
+        const lineSplit = line.replace(/\t/g, "").split(" ");
 
-    /* questions */
-    if (
-      lineSplit[0].match(/^[0-9]/) &&
-      (lineSplit[0].endsWith('.') || lineSplit[0].endsWith(')'))
-    ) {
-      question_id = lineSplit[0].slice(0, lineSplit[0].length - 1);
-      alphabetIndex = 0;
+        /* questions */
+        if (
+            lineSplit[0].match(/^[0-9]/) &&
+            (lineSplit[0].endsWith(".") || lineSplit[0].endsWith(")"))
+        ) {
+            question_id = lineSplit[0].slice(0, lineSplit[0].length - 1);
+            alphabetIndex = 0;
 
-      questions.push({
-        question_id,
-        name: lineSplit.slice(1, lineSplit.length).join(' '),
-      });
-    } else {
-      /* options */
-      if (question_id && line.trim() !== '') {
-        // if (line.includes('\t'))
-        //   console.log(
-        //     line,
-        //     lineSplit[0].endsWith('.'),
-        //     console.log(lineSplit[0])
-        //   );
+            questions.push({
+                id: question_id,
+                topics_id: "1",
+                questions: lineSplit.slice(1, lineSplit.length).join(" "),
+            });
+        } else {
+            /* options */
+            if (question_id && line.trim() !== "") {
+                // if (line.includes('\t'))
+                //   console.log(
+                //     line,
+                //     lineSplit[0].endsWith('.'),
+                //     console.log(lineSplit[0])
+                //   );
 
-        const text = lineSplit[0].endsWith('.')
-          ? lineSplit.slice(1, lineSplit.length).join(' ')
-          : line;
+                const text = lineSplit[0].endsWith(".")
+                    ? lineSplit.slice(1, lineSplit.length).join(" ")
+                    : line;
 
-        console.log({
-          question_id,
-          option: alphabet[alphabetIndex],
-          text,
-        });
+                // console.log({
+                //     id: answer_id,
+                //     test_id: question_id,
+                //     option: alphabet[alphabetIndex],
+                //     answer: text,
+                // });
 
-        options.push({
-          question_id,
-          option: alphabet[alphabetIndex],
-          text,
-        });
+                options.push({
+                    id: answer_id,
+                    test_id: question_id,
+                    option: alphabet[alphabetIndex],
+                    answer: text,
+                });
+                answer_id++;
+                alphabetIndex++;
+            }
+        }
+    });
 
-        alphabetIndex++;
-      }
-    }
-  });
-
-  lineReader.on('close', () => callback(questions, options));
+    lineReader.on("close", () => callback(questions, options));
 };
 
 parseQuestions((questions, options) => {
-  const questionsParsed = JSON.stringify(questions);
-  const optionsParsed = JSON.stringify(options);
-  fs.writeFileSync('questions_parsed.json', questionsParsed);
-  fs.writeFileSync('options_parsed.json', optionsParsed);
+    const questionsParsed = JSON.stringify(questions);
+    const optionsParsed = JSON.stringify(options);
+    fs.writeFileSync("questions_parsed_test.json", questionsParsed);
+    fs.writeFileSync("options_parsed_test.json", optionsParsed);
 });
